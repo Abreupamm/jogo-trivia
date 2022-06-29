6f1500d95f7d16108db1753d6caa0b155eb9c458
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-06-29 18:47:43 -0300

    Requisito 03 - Correção

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 import logo from '../../trivia.png';
 import '../../App.css';
 
-import { ButtonSettings } from '..';
+import {
+  ButtonSettings,
+} from '../index';
 
 const Header = () => (
   <header className="App-header">
