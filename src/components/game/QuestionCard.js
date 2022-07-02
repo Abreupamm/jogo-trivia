d7043116e7f2c82f4fcc0ebd5e5df8d10f2c5246
Pamela <abrepamm@gmail.com>
2022-07-02 20:27:45 -0300

    questão 7 feita

--- src/components/game/QuestionCard.js
+++ src/components/game/QuestionCard.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import './QuestionCard.css';
 
 // import BooleanQuestion from './BooleanQuestion';
 // import MultipleQuestion from './MultipleQuestion';
 
 const QuestionCard = ({ question }) => {
+  const [clicked, setClicked] = useState(false);
   const randomSort = 0.5;
   const answers = [
     question.correct_answer,
     ...question.incorrect_answers,
   ].sort(() => randomSort - Math.random());
 
+  const handeOnClick = () => {
+    setClicked(true);
+    console.log('true');
+  };
+
+  const addStyle = (answer) => {
+    if (question.correct_answer === answer) {
+      return 'correct';
+    }
+    return 'incorrect';
+  };
   return (
     <div id="question-card">
       <h1 data-testid="question-category">
@@ -22,6 +35,7 @@ const QuestionCard = ({ question }) => {
       <div id="answers" data-testid="answer-options">
         { answers.map((answer, index) => (
           <button
+            className={ clicked ? addStyle(answer) : 'response' }
             type="button"
             key={ index }
             data-testid={
@@ -29,6 +43,7 @@ const QuestionCard = ({ question }) => {
                 ? 'correct-answer'
                 : `wrong-answer=${index}`
             }
+            onClick={ handeOnClick }
           >
             { answer }
           </button>
