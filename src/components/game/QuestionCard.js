e9fabd6dd276ac2e35bf78e88e70c73624b91dbb
Pamela <abrepamm@gmail.com>
2022-07-02 20:41:34 -0300

    lint

--- src/components/game/QuestionCard.js
+++ src/components/game/QuestionCard.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './QuestionCard.css';
-
 // import BooleanQuestion from './BooleanQuestion';
 // import MultipleQuestion from './MultipleQuestion';
 
