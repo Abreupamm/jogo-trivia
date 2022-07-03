b2a467a7381bc01df2db447198c163b7c0595f43
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-07-03 13:08:44 -0300

    Requisito 08 - Concluído

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from './layout/Header';
 export { default as LoginForm } from './login/LoginForm';
 
 export { default as QuestionCard } from './game/QuestionCard';
+export { default as Timer } from './game/Timer';
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,14 +5,18 @@ import {
 
 import {
   SET_QUESTIONS,
+  SET_TIMEOUT,
   fetchQuestions,
   actSetQuestions,
+  actSetTimeOut,
 } from './gameActions';
 
 export {
   SET_PLAYER,
   actSetPlayer,
   SET_QUESTIONS,
+  SET_TIMEOUT,
   actSetQuestions,
   fetchQuestions,
+  actSetTimeOut,
 };
