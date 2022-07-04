f8178ba66745b272278c1016aecc82eff54a62e4
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2022-07-04 13:49:28 -0300

    Requisito 12 feito

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import HeaderGame from '../components/layout/HeaderGame';
 
-class Feedback extends React.Component {
-  render() {
-    return (
-      <div>
-        <HeaderGame />
-      </div>
-    );
-  }
-}
+const Feedback = () => (
+  <div>
+    <HeaderGame />
+  </div>
+);
 
 export default Feedback;
